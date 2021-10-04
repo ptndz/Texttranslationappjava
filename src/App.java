7959965d13d306013c953ce20e8773aad1e81b07
ptn1411 <57529765+ptn1411@users.noreply.github.com>
2021-10-04 18:54:33 +0700

    designer

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class App {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JButton languageSwapButton;
+    private JButton deleteButton;
+    private JButton TRANSLATEButton;
+    private JEditorPane editorPane1;
+    private JEditorPane editorPane2;
+    private JButton historyButton;
+}
