b2059a6dc685b3e5755824f9a21ea9366d319eff
ptn1411 <phamthanhnamdev@gmail.com>
2021-10-07 20:31:14 +0700

    chinh sua table history

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -19,10 +20,11 @@ public class App {
     private JEditorPane editorPane2;
     private JButton historyButton;
     private JTable table1;
+    private JPanel historyjpanel;
     public JComboBox listKeyLanguage;
     public String[] langKey;
-    public static String[] getList( String filePath) throws IOException
-    {
+
+    public static String[] getList(String filePath) throws IOException {
         String[] list = {};
         String url = filePath;
         // Đọc dữ liệu từ File với Scanner
@@ -44,7 +46,8 @@ public class App {
         }
         return list;
     }
-//    public  static  String capitalize(String name)
+
+    //    public  static  String capitalize(String name)
 //    {
 //        String s_uper;
 //        String firstLetter = name.substring(0, 1);
@@ -71,29 +74,61 @@ public class App {
         in.close();
         return response.toString();
     }
-    public static void infoBox(String infoMessage, String titleBar)
-    {
+
+    public static void infoBox(String infoMessage, String titleBar) {
         JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
     }
-    public  App()
-    {
 
+    public App() {
+        //table history
+        
+        Object[] columns = {"", "From", "To"};
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(columns);
 
-        try
-        {
-            String[]  listLanguage = getList("data\\languagelist.txt");
+        // set the model to the table
+
+        table1.setModel(model);
+
+        // quy dinh bang co bao nhieu cot
+        Object[] row = new Object[3];
+
+        row[0] = "1";
+        row[1] = "2";
+        row[2] = "3";
+
+        // add row to the model
+        model.addRow(row);
+
+        //table history hide
+        historyjpanel.setVisible(false);
+
+        historyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (historyjpanel.isVisible()) {
+                    //table history hide
+                    historyjpanel.setVisible(false);
+                } else {
+                    //table history show
+                    historyjpanel.setVisible(true);
+                }
+            }
+        });
+
+        //table history end
+
+        try {
+            String[] listLanguage = getList("data\\languagelist.txt");
             String[] listKeyLanguage1 = getList("data\\languagekeylist.txt");
             langKey = listKeyLanguage1;
 
-            for (String item :listLanguage)
-            {
+            for (String item : listLanguage) {
                 fromComboBox.addItem(item);
                 comboBox2.addItem(item);
             }
-        }
-        catch (IOException ex)
-        {
-            infoBox("Đã xảy ra lỗi !!!","Eror");
+        } catch (IOException ex) {
+            infoBox("Đã xảy ra lỗi !!!", "Eror");
         }
 
         fromComboBox.setMaximumRowCount(10);
@@ -106,32 +141,27 @@ public class App {
             public void actionPerformed(ActionEvent e) {
                 String a = langKey[fromComboBox.getSelectedIndex()];
                 String b = langKey[comboBox2.getSelectedIndex()];
-                String text =editorPane1.getText();
+                String text = editorPane1.getText();
                 try {
                     String translatedText = translate(a, b, text);
                     editorPane2.setText(translatedText);
-                }
-                catch (Exception IOException)
-                {
-                    infoBox("Đã xảy ra lỗi, vui lòng nhập lại","Eror");
+                } catch (Exception IOException) {
+                    infoBox("Đã xảy ra lỗi, vui lòng nhập lại", "Eror");
                 }
 
             }
         });
 
 
-
-
-
-
-
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         JFrame frame = new JFrame("APP");
         frame.setContentPane(new App().panel1);
+        frame.setSize(1000, 700);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+
         frame.setVisible(true);
 
     }
